f2ba34421fbf4dff11164a5297876ac79a299892
Luke Gustafson <lukeg3@gmail.com>
2023-12-25 00:39:10 -0500

    Adding v3 and v4

---
+++ auroch_v3_sanitized.js
@@ -0,0 +1,2 @@
+for(w=e=i=2;i%845;w+=i++%65?(e&15).toString(16):`
+`)e=e*9.1^`COMPRESSED_DATA`.charCodeAt(i/2);console.log(w)
---
+++ auroch_v4_sanitized.js
@@ -0,0 +1,2 @@
+for(w=e=i=``;c=`COMPRESSED_DATA`.charCodeAt(i/2);w+=++i%65?(e&15).toString(16):`
+`)e=e*9.1^c;console.log(w)
